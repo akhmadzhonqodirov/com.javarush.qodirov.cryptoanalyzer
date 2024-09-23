21d516004bc5776bcd711ee128c6b2e6c9776ecd
qodirov <akhmadzhon.qodirov@gmail.com>
2024-09-24 00:09:44 +0500

    Final commit

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,23 +1,31 @@
 public class Alphabet {
-    // Здесь создаём приватную константу ALPHABET, которая содержит буквы русского алфавита в нижнем и верхнем
-    // регистрах, а также спецсимволы
-    private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
-            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
-            " .,!?\"'-:;()";
+    // Приватная константа ALPHABET, которая содержит буквы русского алфавита и спецсимволы
+    private static final char[] ALPHABET = {
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
+            'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
+            'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«',
+            '»', '"', '\'', ':', '!', '?', ' '
+    };
 
     public String getAlphabet() {
-        return ALPHABET;
+        return new String(ALPHABET);
     }
 
     public int getIndex(char character) {
-        return ALPHABET.indexOf(character);
+        for (int i = 0; i < ALPHABET.length; i++) {
+            if (ALPHABET[i] == character) {
+                return i;
+            }
+        }
+        return -1; // Символ не найден
     }
 
     public char getCharacter(int index) {
-        if (index < 0 || index >= ALPHABET.length()) {
+        if (index < 0 || index >= ALPHABET.length) {
             throw new IllegalArgumentException("Индекс вне диапазона");
         }
-        return ALPHABET.charAt(index);
+        return ALPHABET[index];
     }
 
     public char shiftCharacter(char character, int shift) {
@@ -25,7 +33,39 @@ public class Alphabet {
         if (index == -1) {
             return character; // Неизменяем символы, которых нет в алфавите
         }
-        int newIndex = (index + shift + ALPHABET.length()) % ALPHABET.length();
+        int newIndex = (index + shift + ALPHABET.length) % ALPHABET.length;
         return getCharacter(newIndex);
     }
 }
+
+//public class Alphabet {
+//    // Здесь создаём приватную константу ALPHABET, которая содержит буквы русского алфавита в нижнем и верхнем
+//    // регистрах, а также спецсимволы
+//    private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+//            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+//            " .,!?\"'-:;()";
+//
+//    public String getAlphabet() {
+//        return ALPHABET;
+//    }
+//
+//    public int getIndex(char character) {
+//        return ALPHABET.indexOf(character);
+//    }
+//
+//    public char getCharacter(int index) {
+//        if (index < 0 || index >= ALPHABET.length()) {
+//            throw new IllegalArgumentException("Индекс вне диапазона");
+//        }
+//        return ALPHABET.charAt(index);
+//    }
+//
+//    public char shiftCharacter(char character, int shift) {
+//        int index = getIndex(character);
+//        if (index == -1) {
+//            return character; // Неизменяем символы, которых нет в алфавите
+//        }
+//        int newIndex = (index + shift + ALPHABET.length()) % ALPHABET.length();
+//        return getCharacter(newIndex);
+//    }
+//}
