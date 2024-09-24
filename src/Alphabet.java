5376f59cada70950de3d39e7621e7c47bb8faf01
qodirov <akhmadzhon.qodirov@gmail.com>
2024-09-24 19:37:51 +0500

    Final commit

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -38,34 +38,3 @@ public class Alphabet {
     }
 }
 
-//public class Alphabet {
-//    // Здесь создаём приватную константу ALPHABET, которая содержит буквы русского алфавита в нижнем и верхнем
-//    // регистрах, а также спецсимволы
-//    private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
-//            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
-//            " .,!?\"'-:;()";
-//
-//    public String getAlphabet() {
-//        return ALPHABET;
-//    }
-//
-//    public int getIndex(char character) {
-//        return ALPHABET.indexOf(character);
-//    }
-//
-//    public char getCharacter(int index) {
-//        if (index < 0 || index >= ALPHABET.length()) {
-//            throw new IllegalArgumentException("Индекс вне диапазона");
-//        }
-//        return ALPHABET.charAt(index);
-//    }
-//
-//    public char shiftCharacter(char character, int shift) {
-//        int index = getIndex(character);
-//        if (index == -1) {
-//            return character; // Неизменяем символы, которых нет в алфавите
-//        }
-//        int newIndex = (index + shift + ALPHABET.length()) % ALPHABET.length();
-//        return getCharacter(newIndex);
-//    }
-//}
